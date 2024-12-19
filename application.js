03a0e4f579b67f620177b956183af173c0253789
sahroura47 <166107845+sahroura47@users.noreply.github.com>
2024-12-19 13:56:07 +0100

    filter section

--- application.js
+++ application.js
@@ -1,30 +1,70 @@
 
-const API_URL= "http://localhost:5678/api/"
+const API_URL = "http://localhost:5678/api/"
 
-const fetchData= async (resource) => {
-    const response = await fetch(API_URL+ resource);
-    console.log(response);
-    return  await response.json();
+const fetchData = async (resource) => {
+    const response = await fetch(API_URL + resource);
+    return await response.json();
 
 }
 
 
-/*filtres*/ 
+/*filtres*/
+
 const buttonContainer = document.querySelector(".filters");
-async function displayButtons  ()  {
-    console.log(buttonContainer);
-    const buttons = await fetchData("categories");
-    
-    for (i=0; i<buttons.length ; i++){
-        const buttonData= buttons[i].name;
+async function displayButtons() {
+    const categoryList = await fetchData("categories");
+
+    for (i = 0; i < categoryList.length; i++) {
+        const category = categoryList[i];
         const filterbutton = document.createElement('button');
-        filterbutton.innerHTML = buttons[i].name;;
+        filterbutton.dataset.category = category.id;
+        filterbutton.innerHTML = category.name;
         filterbutton.classList.add('filter-button');
         buttonContainer.appendChild(filterbutton);
 
     }
-    
+
 
 }
-
 displayButtons();
+
+
+const listContainer = document.querySelector(".gallery");
+const buttons = document.querySelectorAll('.filter-button');
+console.log(buttons);
+buttonContainer.addEventListener("click", (event) => {
+    if (event.target.tagName === 'BUTTON') {
+        const buttonActive = document.querySelector(".filter-button.active");
+        buttonActive.classList.remove('active');
+        event.target.classList.add('active');
+        displayList(event.target.dataset.category);
+    }
+
+});
+
+
+
+async function displayList(category = null) {
+    let projects = await fetchData("works");
+    if (category != null && category != "" )  {
+        projects= projects.filter(project => {
+         return project.category.id==category;
+        })
+
+    }
+    listContainer.innerHTML = "";
+    for (let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+        const figure = document.createElement("figure");
+        const projectsImg = document.createElement("img");
+        const figureCaption = document.createElement("figcaption");
+        figure.appendChild(projectsImg);
+        figure.appendChild(figureCaption);
+        listContainer.appendChild(figure);
+        projectsImg.src = project.imageUrl;
+        projectsImg.alt = "image chargée par l'API";
+        figureCaption.innerHTML = project.title;
+
+    }
+}
+displayList(); 
