7e20b25dcf3a067132cf8842c5be0981609f5f71
sahroura47 <166107845+sahroura47@users.noreply.github.com>
2025-01-17 13:21:09 +0100

    ajout de ficher js de login et de la page html d'accueil de redirection

---
+++ login.js
@@ -0,0 +1,35 @@
+const API_URL = "http://localhost:5678/api/"
+
+
+document.getElementById("loginForm").addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("emailAddress").value;
+    const password = document.getElementById("password").value;
+    const errorMessage = document.getElementById("error-message");
+
+    try {
+        const response = await fetch(API_URL + "users/login", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password
+            })
+        });
+        const data = await response.json();
+        if (response.ok){
+            localStorage.setItem('authToken', data.token);
+            window.location.href='/newHomePage.html';
+        }
+        if (!response.ok) {
+            throw new Error()
+
+        }
+
+    } catch (error) {
+        errorMessage.innerText = 'Email ou mot de passe invalide!';
+    }
+});
